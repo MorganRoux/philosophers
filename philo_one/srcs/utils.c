593be38510889c9fcafba1875bf4122a6abd07e0
Morgan Roux <roux.morgan@gmail.com>
2021-03-24 21:24:17 +0100

    delete prints.c

--- philo_one/srcs/utils.c
+++ philo_one/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 18:44:25 by mroux             #+#    #+#             */
-/*   Updated: 2021/03/14 18:45:10 by mroux            ###   ########.fr       */
+/*   Updated: 2021/03/24 21:23:48 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,16 @@ void	ft_usleep(unsigned long n)
 			break ;
 	}
 }
+
+void			print_death(int philo_number, unsigned long death_time, t_global *gl)
+{
+	pthread_mutex_lock(&gl->mutex_print);
+	printf("Philo %d died at %lu\n",  philo_number, death_time);
+}
+
+void			print_end(int max_meal, unsigned long end_time, t_global *gl)
+{
+	pthread_mutex_lock(&gl->mutex_print);
+	printf("All Philosphers ate %d meals at %lu.\n",  max_meal, end_time);
+}
+
