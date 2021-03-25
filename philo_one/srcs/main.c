31b6321e1d0d82806405d6fa63afd8c3cda6d8c5
Morgan Roux <roux.morgan@gmail.com>
2021-03-25 20:04:55 +0100

    check checker

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 21:55:44 by mroux             #+#    #+#             */
-/*   Updated: 2021/03/25 19:59:35 by mroux            ###   ########.fr       */
+/*   Updated: 2021/03/25 20:04:47 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,7 @@ void			start_checker(t_global *gl)
 			if ((time = (timeval_to_ms(&now) - timeval_to_ms(&gl->philos[i].last_lunch))) >= gl->time_to_die)
 			{
 				printf("==%lu==\n", timeval_to_ms(&now) - timeval_to_ms(&gl->philos[i].last_lunch));
+				printf("==%ld==\n", (long)(timeval_to_ms(&now) - timeval_to_ms(&gl->philos[i].last_lunch)));
 				print_death(gl->philos[i].philo_number, timeval_to_ms(&now)- gl->philos[i].started_at, gl);
 				return ;
 			}
