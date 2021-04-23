18daf5c3254890f28da62d876ce6e223febafa1b
Morgan@VM <mroux@student.42.fr>
2021-04-23 15:35:01 +0200

    philo 2 wip

--- philo_two/srcs/main.c
+++ philo_two/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 21:55:44 by mroux             #+#    #+#             */
-/*   Updated: 2021/04/15 15:32:40 by mroux            ###   ########.fr       */
+/*   Updated: 2021/04/23 15:33:08 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,26 +44,22 @@ void		launch(t_global *gl, int i,
 	args[i].philo = &gl->philos[i];
 	pthread_create(&(gl->philos[i].thread_id), NULL, &philo_thread, &args[i]);
 	pthread_detach(gl->philos[i].thread_id);
-	usleep(5);
 }
 
-int			start_philos(t_global *gl)
+int			start_philos(t_global *gl, t_thread_args *args)
 {
 	int				i;
 	struct timeval	now;
-	t_thread_args	*args;
 
 	i = 0;
 	gettimeofday(&now, NULL);
-	if (!(args = (t_thread_args*)
-			malloc(gl->number_of_philos * sizeof(t_thread_args))))
-		return (-1);
 	while (i < gl->number_of_philos)
 	{
 		if (i % 2 == 0)
 			launch(gl, i, &now, args);
 		i++;
 	}
+	ft_usleep(5000);
 	i = 0;
 	while (i < gl->number_of_philos)
 	{
@@ -84,6 +80,7 @@ void		kill_philos(t_global *gl)
 		gl->philos[i].status = 0;
 		pthread_join(gl->philos[i++].thread_id, NULL);
 	}
+	free(gl->philos);
 	sem_unlink("philo_forks");
 	sem_unlink("philo_print");
 }
@@ -91,6 +88,7 @@ void		kill_philos(t_global *gl)
 int			main(int argc, char *argv[])
 {
 	t_global		gl;
+	t_thread_args	*args;
 
 	if (check_vars(argc, argv) == 0)
 	{
@@ -99,8 +97,12 @@ int			main(int argc, char *argv[])
 	}
 	if (init(&gl, argc, argv) == -1)
 		return (0);
-	start_philos(&gl);
+	if (!(args = (t_thread_args*)
+		malloc(gl.number_of_philos * sizeof(t_thread_args))))
+		return (0);
+	start_philos(&gl, args);
 	start_checker(&gl);
 	kill_philos(&gl);
+	free(args);
 	return (0);
 }
